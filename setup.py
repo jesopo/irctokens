3706d55133ae27777e72868fd1b4b12ceb024c40
jesopo <github@lolnerd.net>
2020-03-11 12:35:52 +0000

    add pypi setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
+
+setuptools.setup(
+    name="irctokens",
+    version=version,
+    author="jesopo",
+    author_email="pip@jesopo.uk",
+    description="RFC1459 and IRCv3 protocol tokeniser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jesopo/irctokens",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Communications :: Chat :: Internet Relay Chat"
+    ],
+    python_requires='>=3.5',
+)
