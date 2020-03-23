a3b55f6292c2343075bb56b5be6bcb2ce6d09211
jesopo <github@lolnerd.net>
2020-03-23 16:48:47 +0000

    add test/parser_tests.py, from github:ircdocs/parser-tests

--- test/__init__.py
+++ test/__init__.py
@@ -2,4 +2,5 @@ from .tokenise        import *
 from .format          import *
 from .stateful_decode import *
 from .stateful_encode import *
-from .hostmask import *
+from .hostmask        import *
+from .parser_tests    import *
---
+++ test/parser_tests.py
@@ -0,0 +1,26 @@
+import os.path, unittest
+import yaml
+import irctokens
+
+# run test cases sourced from:
+# https://github.com/ircdocs/parser-tests
+
+dir      = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(dir, "_data")
+
+class ParserTestsSplit(unittest.TestCase):
+    def test_split(self):
+        data_path = os.path.join(data_dir, "msg-split.yaml")
+        with open(data_path) as data_file:
+            tests = yaml.safe_load(data_file.read())["tests"]
+
+        for test in tests:
+            input = test["input"]
+            atoms = test["atoms"]
+
+            tokens = irctokens.tokenise(input)
+
+            self.assertEqual(tokens.tags,    atoms.get("tags", None))
+            self.assertEqual(tokens.source,  atoms.get("source", None))
+            self.assertEqual(tokens.command, atoms["verb"].upper())
+            self.assertEqual(tokens.params,  atoms.get("params", []))
