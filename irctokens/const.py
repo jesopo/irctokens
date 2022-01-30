41eee7e9978a67a03821707c28d7c2a22ba7b80a
A_D <aunderscored@gmail.com>
2022-01-30 17:31:34 +0200

    use immutable types for constants

--- irctokens/const.py
+++ irctokens/const.py
@@ -1,2 +1,2 @@
-TAG_UNESCAPED = ["\\",   " ",   ";",   "\r",  "\n"]
-TAG_ESCAPED   = ["\\\\", "\\s", "\\:", "\\r", "\\n"]
+TAG_UNESCAPED = ("\\", " ", ";", "\r", "\n")
+TAG_ESCAPED = ("\\\\", "\\s", "\\:", "\\r", "\\n")
