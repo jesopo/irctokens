abe0af47d600618655b16b0a85a6c8196e204700
A_D <aunderscored@gmail.com>
2022-01-30 17:42:23 +0200

    use 3.10 annotations, remove object subclass

--- irctokens/stateful.py
+++ irctokens/stateful.py
@@ -1,8 +1,10 @@
-from typing import List, Optional
-from .line  import Line, tokenise
+from __future__ import annotations
 
-class StatefulDecoder(object):
-    def __init__(self, encoding: str="utf8", fallback: str="latin-1"):
+from .line import Line, tokenise
+
+
+class StatefulDecoder:
+    def __init__(self, encoding: str = "utf8", fallback: str = "latin-1"):
         self._encoding = encoding
         self._fallback = fallback
         self.clear()
@@ -13,27 +15,29 @@ class StatefulDecoder(object):
     def pending(self) -> bytes:
         return self._buffer
 
-    def push(self, data: bytes) -> Optional[List[Line]]:
+    def push(self, data: bytes) -> list[Line] | None:
         if not data:
             return None
 
         self._buffer += data
-        lines_b = [l.strip(b"\r") for l in self._buffer.split(b"\n")]
+        lines_b = [ln.strip(b"\r") for ln in self._buffer.split(b"\n")]
         self._buffer = lines_b.pop(-1)
 
-        lines: List[Line] = []
+        lines: list[Line] = []
         for line in lines_b:
             lines.append(tokenise(line, self._encoding, self._fallback))
+
         return lines
 
-class StatefulEncoder(object):
-    def __init__(self, encoding: str="utf8"):
+
+class StatefulEncoder:
+    def __init__(self, encoding: str = "utf8"):
         self._encoding = encoding
         self.clear()
 
     def clear(self):
         self._buffer = b""
-        self._buffered_lines: List[Line] = []
+        self._buffered_lines: list[Line] = []
 
     def pending(self) -> bytes:
         return self._buffer
@@ -45,4 +49,5 @@ class StatefulEncoder(object):
     def pop(self, byte_count: int):
         sent = self._buffer[:byte_count].count(b"\n")
         self._buffer = self._buffer[byte_count:]
+
         return [self._buffered_lines.pop(0) for _ in range(sent)]
