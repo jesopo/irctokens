0b83008cdfac495f7d57cf034a8bb3276af88f2c
A_D <aunderscored@gmail.com>
2022-01-30 17:40:06 +0200

    use 3.10 (3.7 compatible) annotations

--- irctokens/formatting.py
+++ irctokens/formatting.py
@@ -1,29 +1,37 @@
-from typing import Dict, List, Optional
+from __future__ import annotations
+
 from .const import TAG_ESCAPED, TAG_UNESCAPED
+
 
 def _escape_tag(value: str):
     for i, char in enumerate(TAG_UNESCAPED):
         value = value.replace(char, TAG_ESCAPED[i])
+
     return value
 
+
 def format(
-        tags:    Optional[Dict[str, str]],
-        source:  Optional[str],
-        command: str,
-        params:  List[str]):
-    outs: List[str] = []
+    tags: dict[str, str] | None,
+    source: str | None,
+    command: str,
+    params: list[str],
+):
+    outs: list[str] = []
     if tags:
         tags_str = []
         for key in sorted(tags.keys()):
             if tags[key]:
                 value = tags[key]
                 tags_str.append(f"{key}={_escape_tag(value)}")
+
             else:
                 tags_str.append(key)
+
         outs.append(f"@{';'.join(tags_str)}")
 
     if source is not None:
         outs.append(f":{source}")
+
     outs.append(command)
 
     params = params.copy()
@@ -32,13 +40,15 @@ def format(
         for param in params:
             if " " in param:
                 raise ValueError("non last params cannot have spaces")
+
             elif param.startswith(":"):
                 raise ValueError("non last params cannot start with colon")
+
         outs.extend(params)
 
-        if (not last or
-                " " in last or
-                last.startswith(":")):
+        if not last or " " in last or last.startswith(":"):
             last = f":{last}"
+
         outs.append(last)
+
     return " ".join(outs)
