0da59938234cc552d68564c378eace6f7e53f0cc
A_D <aunderscored@gmail.com>
2022-01-30 17:30:40 +0200

    Add __all__ to silence private import warnings

--- irctokens/__init__.py
+++ irctokens/__init__.py
@@ -1,4 +1,13 @@
-from .line     import Line, build, tokenise
 from .hostmask import Hostmask, hostmask
+from .line import Line, build, tokenise
 from .stateful import StatefulDecoder, StatefulEncoder
 
+__all__ = [
+    "Line",
+    "build",
+    "tokenise",
+    "Hostmask",
+    "hostmask",
+    "StatefulDecoder",
+    "StatefulEncoder",
+]
